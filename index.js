e2d8837fdc355359be13d70e7970014ea25c83f8
Pavas Garg <pavasgarg2003@gmail.com>
2022-08-26 21:23:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,20 @@ const Contact = require('./models/contact');
 const app = express();
 
 /*
-<---------------------------------------------------------------------------------------------------->
+<-------------------------------------------------------------------------------------------------------------------------------------------------------->
 we need to use body parser as the data recieved by the endpoint on submitting the form is
 encoded and we need to convert it to json so that we can pass on further
 here bodyParser is acting as a middleware
-<---------------------------------------------------------------------------------------------------->
+<-------------------------------------------------------------------------------------------------------------------------------------------------------->
 */
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static("assets"));
 
 /*
-<---------------------------------------------------------------------------------------------------->
-next in middleware is used to tell the app where to go next that is next searches more middlewares if present and iterates over every middleware and at last goes to the endpoint on which request is created.
+<-------------------------------------------------------------------------------------------------------------------------------------------------------->
+next in middleware is used to tell the app where to go next that is next searches more middlewares if present and iterates over every middleware and
+at last goes to the endpoint on which request is created.
 custom middleware1
 app.use(function(req,res,next){
     console.log("middleware1 called");
@@ -30,7 +31,8 @@ app.use(function(req,res,next){
     console.log("middleware2 called");
     next();
 });
-<---------------------------------------------------------------------------------------------------->*/
+<-------------------------------------------------------------------------------------------------------------------------------------------------------->
+*/
 
 app.set("view engine","ejs"); // setting the template engine for this express app as EJS.
 app.set("views",path.join(__dirname,'views')); // telling the app where the view files are located
@@ -62,11 +64,11 @@ app.post("/create-contact",(req,res) =>{
 });
 
 /*
-<---------------------------------------------------------------------------------------------------->
+<-------------------------------------------------------------------------------------------------------------------------------------------------------->
 passing params in the url
 params can be normal params as well as query params
 query params
-<---------------------------------------------------------------------------------------------------->
+<-------------------------------------------------------------------------------------------------------------------------------------------------------->
 */
 
 app.get("/delete-contact/",(req,res) =>{
@@ -80,7 +82,7 @@ app.get("/delete-contact/",(req,res) =>{
 });
 
 /*
-<---------------------------------------------------------------------------------------------------->
+<-------------------------------------------------------------------------------------------------------------------------------------------------------->
 params
 app.get("/delete-contact/:id",(req,res) =>{
     var id = req.params.id;
@@ -92,7 +94,7 @@ app.get("/delete-contact/:id",(req,res) =>{
     }
     res.redirect("/");
 });
-<---------------------------------------------------------------------------------------------------->
+<-------------------------------------------------------------------------------------------------------------------------------------------------------->
 */
 
 app.listen(port,(err) =>{
