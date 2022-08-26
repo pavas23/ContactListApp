1f6a5f81b56350beba99abc3c5769f35f14e6a5c
Pavas Garg <pavasgarg@Pavass-MacBook-Pro.local>
2022-08-26 21:19:26 +0530

    added comments

--- index.js
+++ index.js
@@ -6,30 +6,34 @@ const db = require("./config/mongoose");
 const Contact = require('./models/contact');
 const app = express();
 
-/* we need to use body parser as the data recieved by the endpoint on submitting the form is
-encoded and we need to convert it to json so that we can pass on further.*/
-// here bodyParser is acting as a middleware
+/*
+<---------------------------------------------------------------------------------------------------->
+we need to use body parser as the data recieved by the endpoint on submitting the form is
+encoded and we need to convert it to json so that we can pass on further
+here bodyParser is acting as a middleware
+<---------------------------------------------------------------------------------------------------->
+*/
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static("assets"));
 
-// next in middleware is used to tell the app where to go next that is next searches more middlewares //if present and iterates over every middleware and at last goes to the endpoint on which request is //created.
-
-// custom middleware1
-// app.use(function(req,res,next){
-//     console.log("middleware1 called");
-//     next();
-// });
-
-// // middleware2
-// app.use(function(req,res,next){
-//     console.log("middleware2 called");
-//     next();
-// });
+/*
+<---------------------------------------------------------------------------------------------------->
+next in middleware is used to tell the app where to go next that is next searches more middlewares if present and iterates over every middleware and at last goes to the endpoint on which request is created.
+custom middleware1
+app.use(function(req,res,next){
+    console.log("middleware1 called");
+    next();
+});
+// middleware2
+app.use(function(req,res,next){
+    console.log("middleware2 called");
+    next();
+});
+<---------------------------------------------------------------------------------------------------->*/
 
 app.set("view engine","ejs"); // setting the template engine for this express app as EJS.
 app.set("views",path.join(__dirname,'views')); // telling the app where the view files are located
-
-
 
 app.get("/",(req,res) =>{
     Contact.find({},function(err,contacts){
@@ -57,10 +61,14 @@ app.post("/create-contact",(req,res) =>{
     return res.redirect("/");
 });
 
+/*
+<---------------------------------------------------------------------------------------------------->
+passing params in the url
+params can be normal params as well as query params
+query params
+<---------------------------------------------------------------------------------------------------->
+*/
 
-// passing params in the url
-// params can be normal params as well as query params
-// query params
 app.get("/delete-contact/",(req,res) =>{
     var id = req.query._id;
     Contact.findByIdAndDelete(id,(err)=>{
@@ -71,17 +79,21 @@ app.get("/delete-contact/",(req,res) =>{
     return res.redirect("/");
 });
 
-// params
-// app.get("/delete-contact/:id",(req,res) =>{
-//     var id = req.params.id;
-//     for(let i=0;i<contactList.length;i++){
-//         if(contactList[i].id == id){
-//             contactList.splice(i,1);
-//             break;
-//         }
-//     }
-//     res.redirect("/");
-// });
+/*
+<---------------------------------------------------------------------------------------------------->
+params
+app.get("/delete-contact/:id",(req,res) =>{
+    var id = req.params.id;
+    for(let i=0;i<contactList.length;i++){
+        if(contactList[i].id == id){
+            contactList.splice(i,1);
+            break;
+        }
+    }
+    res.redirect("/");
+});
+<---------------------------------------------------------------------------------------------------->
+*/
 
 app.listen(port,(err) =>{
     if(err){
